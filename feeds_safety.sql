bb57a88c7b71aa2d6e0e11b7240f065587b3146f
tamikanormil <74426782+tamikanormil@users.noreply.github.com>
2022-06-13 08:51:53 -0400

    Update feeds_safety.sql

--- feeds_safety.sql
+++ feeds_safety.sql
@@ -19,11 +19,29 @@ with base as
     )
     LEFT JOIN UNNEST(illegal_term.value) value
      WHERE DATE(_PARTITIONTIME) = DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)
-      and listing_state = 'is_active')
-select category, count(distinct listing_id) as listings, count(distinct case when  value = true then listing_id end)
-blocked_listings
-, count(distinct case when  value = true and context in ('brand alignment','reputational') then listing_id end)
-brand_unsafe_listings
-from base a
-LEFT JOIN  `etsy-data-warehouse-prod.etsy_aux.compliance_blocklist_terms` b using (term, region, language)
-group by 1;
+      and listing_state = 'is_active'),
+category_gms as
+  (select *
+  from 
+  (select olf.category as category_opp_sizing, concat(replace(reporting_channel_group,' ',''), '_', replace(replace(engine,' ',''),'-','_')) as reporting_channel_group_engine, sum(coalesce(attr_gms,0)) as attr_gms
+  from `etsy-data-warehouse-dev.rollups.perf_listings_sample_af` af
+  left join (SELECT * FROM `etsy-data-warehouse-prod.olf.olf_hydration_daily` WHERE DATE(_PARTITIONTIME) = "2022-06-12") olf using (listing_id)
+  where date_trunc(date,quarter) = '2022-01-01'
+  group by 1,2) 
+  PIVOT 
+  ( -- #2 aggregate
+   sum(attr_gms) AS q12022_attr_gms
+  -- #3 pivot_column
+  FOR reporting_channel_group_engine in ('PaidSocial_Pinterest_Paid', 'PaidSocial_Facebook_Paid', 'PLA_Connexity', 'PLA_Bing_Paid','Display_Bing_Paid', 'Affiliates_Affiliates', 'PLA_Google_Paid', 'Display_Google_Paid'))
+    ),  
+blocked_base as        
+    (select category, count(distinct listing_id) as listings, count(distinct case when  value = true then listing_id end)
+    blocked_listings
+    , count(distinct case when  value = true and context in ('brand alignment','reputational') then listing_id end)
+    brand_unsafe_listings
+    from base a
+    LEFT JOIN  `etsy-data-warehouse-prod.etsy_aux.compliance_blocklist_terms` b using (term, region, language)
+    group by 1)
+select b.*, c.*
+from  blocked_base b
+left join category_gms c on b.category = c.category_opp_sizing  ;
