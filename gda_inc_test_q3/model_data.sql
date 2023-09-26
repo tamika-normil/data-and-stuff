6040a0d041f9396b6c6fd61b4038f97698c3de1c
tamikanormil <74426782+tamikanormil@users.noreply.github.com>
2023-09-26 12:19:01 -0400

    Update model_data.sql

--- gda_inc_test_q3/model_data.sql
+++ gda_inc_test_q3/model_data.sql
@@ -1,15 +1,59 @@
-with google_spend_data as 
+create temp table keys
+partition by `date`
+cluster by geo as (
+ select distinct `date`, geo
+from etsy-data-warehouse-dev.tnormil.google_metro_dma_perf
+union distinct 
+select distinct date_add(`date`, interval 1 year)  as `date`, geo
+from etsy-data-warehouse-dev.tnormil.google_metro_dma_perf
+where date_add(`date`, interval 1 year) < current_date);
+
+with google_perf as 
+(select
+  k.`date`
+  ,k.geo
+  -- THIS YEAR
+  ,a.response
+  ,a.cost
+  ,a.buyers        
+  -- LAST YEAR
+  ,b.response as response_ly
+  ,b.cost as cost_ly
+  ,b.buyers as buyer_ly 
+  from keys k 
+  left join `etsy-data-warehouse-dev.tnormil.google_metro_dma_perf` a
+    on k.`date` = a.date
+    and k.geo = a.geo
+  left join 
+   (select date_add(`date`, interval 1 year) as date1year   -- this data needs to be grouped bec 1 year past 2/28 and 2/29 is same
+        ,geo
+        ,sum(response) as response
+        ,sum(cost) as cost
+        ,sum(buyers) as buyers
+        from `etsy-data-warehouse-dev.tnormil.google_metro_dma_perf`
+        where `date` < date_sub(date_sub(current_date, interval 1 year), interval 1 day)
+        group by 1,2) b  
+    on k.`date` = b.date1year
+    and k.geo = b.geo
+  order by k.date desc),
+google_spend_data as 
     (SELECT g.day as date, l.string_field_2 as geo, sum(case when Account_name
     = 'Etsy SEM US - Branded' then cast(Cost__Converted_currency_
     as float64) else 0 end) as sem_brand_cost
+    , sum(case when Account_name
+    like '% NB %' then cast(Cost__Converted_currency_
+    as float64) else 0 end) as sem_nb_cost,
+    sum(case when lower(Account_name)
+    like '%shopping%' then cast(Cost__Converted_currency_
+    as float64) else 0 end) as pla_cost,
     FROM etsy-data-warehouse-dev.tnormil.google_metro_dma_cost g
     left join etsy-data-warehouse-dev.tnormil.metro_lookup l on g.DMA_Region__Matched_ = l.string_field_0	
     group by 1,2),
 agg_pre_period as 
     (SELECT coalesce(g.geo,cast(gd.geo as string)) as geo, 
     sum(g.buyers) as agg_buyers,
-    sum(g.clicks) as agg_clicks,
-    sum(g.impressions) as agg_impressions,
+    --sum(g.clicks) as agg_clicks,
+    --sum(g.impressions) as agg_impressions,
     sum(gd.buyers) as agg_gda_buyers, 
     sum(gd.clicks) as agg_gda_clicks, 
     sum(gd.impressions) as agg_gda_impressions,
@@ -22,23 +66,26 @@ agg_pre_period as
 SELECT g.date, assignment, g.geo, case when assignment = 'Control' then 1 else 2 end as groupp, 
 
 agg_buyers,
-agg_clicks,
-agg_impressions,
+--agg_clicks,
+--agg_impressions,
 agg_gda_buyers, 
 agg_gda_clicks, 
 agg_gda_impressions,
 
 sum(g.response) as response, sum(g.cost) as cost, 
--- sum(g.buyers) as buyers, sum(g.clicks) as clicks, sum(g.impressions) as impressions,
+sum(g.response_ly) as response_ly, sum(g.cost_ly) as cost_ly, 
 sum(gd.response) as gda_response, sum(gd.cost) as gda_cost, 
--- sum(gd.buyers) as gda_buyers, sum(gd.clicks) as gda_clicks, sum(gd.impressions) as gda_impressions,
-sum(sem_brand_cost) as sem_b_cost
-FROM etsy-data-warehouse-dev.tnormil.google_metro_dma_perf g
+sum(sem_brand_cost) as sem_b_cost,
+sum(sem_nb_cost) as sem_nb_cost,
+sum(pla_cost) as pla_cost
+FROM google_perf g
 left join etsy-data-warehouse-dev.tnormil.design_data_tbr d on g.geo = cast(d.geo as string) 
 left join etsy-data-warehouse-dev.tnormil.gda_metro_dma_perf gd on g.geo = cast(gd.geo as string) and g.date = gd.date
 left join google_spend_data gs on g.geo = gs.geo and g.date = gs.date
 left join agg_pre_period a on g.geo = a.geo
+--left join agg_excluded e on g.date = e.date
 where assignment <> 'Excluded'
 and g.date >= '2023-01-01'
-group by 1,2,3,4,5,6,7,8,9,10
+and g.date <= '2023-08-01'
+group by 1,2,3,4,5,6,7,8
 order by 1 desc;
