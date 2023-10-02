4ba38cd1a49c0d7af2dd602c94d1c09ea14fd6f5
tamikanormil <74426782+tamikanormil@users.noreply.github.com>
2023-10-02 12:29:07 -0400

    Create malachi_seller_acq.sql

---
+++ malachi_seller_acq.sql
@@ -0,0 +1,101 @@
+    with sg_base as (
+          select
+          a.shop_id,
+          a.shop_name,
+          a.user_id,
+          b.country,
+          b.tactic,
+          b.first_date,
+          b.ads_credit_awarded_date,
+          c.promo_code,
+          COUNT(tactic) OVER (PARTITION BY b.user_id) as total_tactics_by_shop
+        from
+        `etsy-data-warehouse-prod.rollups.seller_basics` a
+        inner join
+        `etsy-data-warehouse-prod.rollups.optimised_sellers_upload` b on a.user_id = b.user_id
+        inner join
+        (select user_id, REGEXP_EXTRACT(landing_event_url, r'promo_code=([^&]+)') AS promo_code from `etsy-data-warehouse-prod.weblog.visits` where _date > '2023-09-01') c
+        on a.user_id = c.user_id),
+         sem_base as (
+          select distinct
+          a.shop_id,
+          a.shop_name,
+          a.user_id,
+          c.promo_code,
+          marketing_region as country,
+          'SEM' as tactic,
+          a.open_date,
+          b.date as first_date,
+          null as ads_credit_awarded_date,
+          0 as total_tactics_by_shop
+        from
+        `etsy-data-warehouse-prod.rollups.seller_basics` a
+        inner join
+        `etsy-data-warehouse-prod.rollups.utm_shops` b on a.user_id = b.user_id
+        inner join
+        (select user_id,  REGEXP_EXTRACT(landing_event_url, r'promo_code=([^&]+)') AS promo_code from `etsy-data-warehouse-prod.weblog.visits` where _date > '2023-09-01') c
+        on a.user_id = c.user_id),
+        sem_driven_SG as (
+        select
+        a.shop_id,
+        a.shop_name,
+        a.user_id,
+        a.country,
+        a.tactic,
+        open_date,
+        coalesce(a.promo_code, b.promo_code) as promo_code,
+        case when b.shop_id is not null then 'SEM Driven' else "Not SEM Driven" end as is_SEM_driven,
+        coalesce((cast (a.first_date as date)), b.first_date) as first_date,
+        a.ads_credit_awarded_date,
+        a.total_tactics_by_shop
+        from sg_base a left join sem_base b on a.shop_id = b.shop_id
+        ),
+        sem_only as
+        (
+        select
+        a.*
+        from sem_base a left join sem_driven_SG b on a.shop_id = b.shop_id
+        where b.shop_id is null
+        ),
+        output as (
+        select
+        shop_id,
+        shop_name,
+        promo_code,
+        user_id,
+        country,
+        tactic,
+        open_date,
+        is_SEM_driven,
+        cast(first_date as timestamp) as first_date,
+        cast(ads_credit_awarded_date as timestamp) as ads_credit_awarded_date,
+        total_tactics_by_shop
+        from sem_driven_SG
+        union all
+        select
+        shop_id,
+        shop_name,
+        promo_code,
+        user_id,
+        country,
+        tactic,
+        open_date,
+        'SEM Driven'  as is_SEM_driven,
+        cast(first_date as timestamp) as first_date,
+        null as ads_credit_awarded_date,
+        total_tactics_by_shop
+        from sem_only)
+        select
+        a.*,
+        case when a.shop_id is not null and a.open_date is not null then 1 else 0 end as shops,
+        b.high_potential_seller_status,
+        b.primary_language,
+        b.active_listings,
+        b.about_page,
+        b.seller_intent,
+        b.has_ads_rev,
+        b.top_category_new
+        from output a
+        inner join  `etsy-data-warehouse-prod.rollups.seller_basics` b
+        on a.shop_id = b.shop_id
+        where cast(first_date AS DATE) < current_date;;
