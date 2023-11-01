f81a6773daf1dfd9c0f474d65e8e59ad4caffcc3
tamikanormil <74426782+tamikanormil@users.noreply.github.com>
2023-11-01 14:56:56 -0400

    Update gift_card_purchases.sql

--- gift_cards/gift_card_purchases.sql
+++ gift_cards/gift_card_purchases.sql
@@ -137,6 +137,70 @@ select g.month, g.buyer_type, g.buyers, g.repurchase_30, a.buyers, a.repurchase_
 from gc_purchases g
 left join all_purchases a using (month,buyer_type)
 
+-- buyer type distibution by frequency x target gender
+
+with gms_share_buyer_type as 
+    (select gc.*, r.buyer_type, 
+      CASE when date_diff(gc.purchase_date, r.purchase_date, day) = 0 then r.buyer_type else 
+      CASE 
+        WHEN buyer_type is null THEN 'new_buyer'
+        WHEN date_diff(gc.purchase_date, r.purchase_date, day) >= 365 then 'reactivated_buyer'
+        WHEN purchase_day_number + 1 = 2
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '2x_buyer'
+        WHEN purchase_day_number + 1 = 3
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '3x_buyer'
+        WHEN purchase_day_number + 1 >= 4 and purchase_day_number + 1 <= 9
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '4_to_9x_buyer'
+        WHEN purchase_day_number + 1 >= 10
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '10plus_buyer'
+        ELSE 'other'
+      END END AS buyer_type_new,
+    date_diff(gc.purchase_date, r.purchase_date, day) as days_between,
+    row_number() over (partition by gc.receipt_id order by date_diff(gc.purchase_date, r.purchase_date, day) asc) as rnk
+    from etsy-data-warehouse-dev.tnormil.gc_receipts gc
+    left join receipt_data r on gc.mapped_user_id = r.mapped_user_id and gc.purchase_date >= r.purchase_date
+    qualify rnk = 1 or rnk is null)
+select case when country = 'United States' then 'US' else 'INTL' end as us_intl,
+date_trunc(gc.purchase_date, month) as month, target_gender ,
+case when buyer_type_new = 'new_buyer' then buyer_type_new else 'existing_buyer' end as buyer_type,
+count(distinct gc.receipt_id) as receipts
+from etsy-data-warehouse-dev.tnormil.gc_receipts gc
+left join `etsy-data-warehouse-prod.rollups.buyer_basics` using (mapped_user_id)
+left join gms_share_buyer_type using (receipt_id)
+group by 1,2,3,4;
+
+-- buyer type distibution by frequency x age
+
+with gms_share_buyer_type as 
+    (select gc.*, r.buyer_type, 
+      CASE when date_diff(gc.purchase_date, r.purchase_date, day) = 0 then r.buyer_type else 
+      CASE 
+        WHEN buyer_type is null THEN 'new_buyer'
+        WHEN date_diff(gc.purchase_date, r.purchase_date, day) >= 365 then 'reactivated_buyer'
+        WHEN purchase_day_number + 1 = 2
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '2x_buyer'
+        WHEN purchase_day_number + 1 = 3
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '3x_buyer'
+        WHEN purchase_day_number + 1 >= 4 and purchase_day_number + 1 <= 9
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '4_to_9x_buyer'
+        WHEN purchase_day_number + 1 >= 10
+        AND date_diff(gc.purchase_date, r.purchase_date, day) < 365 THEN '10plus_buyer'
+        ELSE 'other'
+      END END AS buyer_type_new,
+    date_diff(gc.purchase_date, r.purchase_date, day) as days_between,
+    row_number() over (partition by gc.receipt_id order by date_diff(gc.purchase_date, r.purchase_date, day) asc) as rnk
+    from etsy-data-warehouse-dev.tnormil.gc_receipts gc
+    left join receipt_data r on gc.mapped_user_id = r.mapped_user_id and gc.purchase_date >= r.purchase_date
+    qualify rnk = 1 or rnk is null)
+select case when country = 'United States' then 'US' else 'INTL' end as us_intl,
+date_trunc(gc.purchase_date, month) as month, estimated_age ,
+case when buyer_type_new = 'new_buyer' then buyer_type_new else 'existing_buyer' end as buyer_type,
+count(distinct gc.receipt_id) as receipts
+from etsy-data-warehouse-dev.tnormil.gc_receipts gc
+left join `etsy-data-warehouse-prod.rollups.buyer_basics` using (mapped_user_id)
+left join gms_share_buyer_type using (receipt_id)
+group by 1,2,3,4;
+
 --- AOV stuff
 
 -- Past Year Distribution of Gift Cards Purchased by Original Gift Card Value & Key Market
@@ -173,7 +237,7 @@ and transaction_live = 1
 group by 1, 2, 3;
 
 -- Median transaction value of GC purchase by region over time
-
+/*
 SELECT distinct date_trunc(date(aat.creation_tsz), month) as date_month,
 date_trunc(date(aat.creation_tsz), year) as date_year,
 case when marketing_region in ('US','GB','DE','FR','CA') then marketing_region else 'Row' end as key_market, 
@@ -187,3 +251,103 @@ left join etsy-data-warehouse-prod.buyatt_mart.visits v using (visit_id)
 WHERE a.type = 'STANDARD'
 and date(aat.creation_tsz) >= '2021-01-01'
 and transaction_live = 1;
+*/
+
+-- When do gift card purchases peak before holiday?
+-- Peaks are estimated by identifying days where 2 of 3 days before or after the respective day have receipts share dedicated to gift card purchases above benchmarks
+
+with relevant_holiday as 
+(select *
+from etsy-data-warehouse-dev.tnormil.holidays
+where holiday in ("Father's Day",
+"Mother's Day",
+"Christmas Day",
+"Valentine's Day",
+"Boxing Day")),
+daily as
+(select v.marketing_region,
+date(creation_tsz) as purchase_date,
+holiday,
+date_diff(h.date,date(creation_tsz), day) as days_to_holiday,
+count(distinct rg.receipt_id) as receipts,
+count(distinct case when rg.is_gift_card = 1 then rg.receipt_id end) as gc_receipts
+from etsy-data-warehouse-prod.transaction_mart.receipts_gms rg
+left join `etsy-data-warehouse-prod.visit_mart.visits_transactions` vt using (receipt_id)
+left join etsy-data-warehouse-prod.buyatt_mart.visits v using (visit_id)
+left join relevant_holiday h on date(creation_tsz) >= date_sub(h.date, interval 30 day) 
+and date(creation_tsz) <= date_add(h.date, interval 30 day)  
+and v.marketing_region = h.marketing_region
+where v.marketing_region in ('US','CA','FR','GB','DE')
+group by 1,2,3,4),
+non_holiday_history as 
+(select distinct purchase_date, marketing_region, 
+sum(IF(holiday is null, gc_receipts, 0)) OVER (partition by marketing_region ORDER BY purchase_date asc ROWS BETWEEN 180 PRECEDING AND current row) as gc_receipts,
+sum(IF(holiday is null, receipts, 0)) OVER (partition by marketing_region ORDER BY purchase_date asc ROWS BETWEEN 180 PRECEDING AND current row) as receipts,
+from daily),
+daily_history as 
+(select d.*, 
+h.gc_receipts as benchmark_gc_receipts,
+h.receipts as benchmark_receipts,
+safe_divide(h.gc_receipts, h.receipts) as benchmark_share,
+safe_divide(d.gc_receipts, d.receipts) as share_gc,
+COUNTIF( safe_divide(d.gc_receipts, d.receipts) > safe_divide(h.gc_receipts, h.receipts)*1.2 ) OVER (partition by d.marketing_region, d.holiday ORDER BY d.purchase_date desc ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING) as historical_peaks_before,
+COUNTIF( safe_divide(d.gc_receipts, d.receipts) > safe_divide(h.gc_receipts, h.receipts)*1.2  ) OVER (partition by d.marketing_region, d.holiday ORDER BY d.purchase_date asc ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING) as historical_peaks_after,
+from daily d
+left join (select purchase_date, marketing_region, gc_receipts, receipts, row_number() over (partition by purchase_date, marketing_region order by safe_divide(gc_receipts, receipts) desc) as rnk
+from non_holiday_history qualify rnk = 1) h on d.purchase_date = h.purchase_date and d.marketing_region = h.marketing_region)
+select *
+from daily_history
+where holiday is not null
+order by marketing_region, holiday, purchase_date asc;
+
+-- When do gift card purchases peak before holiday?
+-- Another approach, comparing 7 vs 60 day average
+
+/*
+
+with relevant_holiday as 
+(select *, lag(date) over (partition by marketing_region order by date asc) as last_holiday
+from etsy-data-warehouse-dev.tnormil.holidays
+where holiday in ("Father's Day",
+"Mother's Day",
+"Christmas Day",
+"Valentine's Day",
+"Boxing Day")),
+daily as
+(select v.marketing_region,
+date(creation_tsz) as purchase_date,
+holiday,
+date_diff(h.date,date(creation_tsz), day) as days_to_holiday,
+count(distinct rg.receipt_id) as receipts,
+count(distinct case when rg.is_gift_card = 1 then rg.receipt_id end) as gc_receipts
+from etsy-data-warehouse-prod.transaction_mart.receipts_gms rg
+#left join etsy-data-warehouse-dev.tnormil.gc_receipts gc using (receipt_id)
+left join etsy-data-warehouse-prod.visit_mart.visits_transactions vt using (receipt_id)
+left join etsy-data-warehouse-prod.buyatt_mart.visits v using (visit_id)
+left join relevant_holiday h on date(creation_tsz) >= date_sub(h.date, interval 30 day) 
+#and date(creation_tsz) >= last_holiday
+and date(creation_tsz) <= date_add(h.date, interval 14 day)  
+and v.marketing_region = h.marketing_region
+where v.marketing_region in ('US','CA','FR','GB','DE')
+group by 1,2,3,4),
+daily_history as 
+(select d.*, 
+SUM(gc_receipts)
+  OVER ( partition by holiday, marketing_region ORDER BY purchase_date asc ROWS BETWEEN 60 Preceding AND Current row) AS gc_receipts_60day,
+SUM(receipts)
+  OVER ( partition by holiday, marketing_region ORDER BY purchase_date asc ROWS BETWEEN 60 Preceding AND Current row) AS receipts_60day,
+SUM(gc_receipts)
+  OVER ( partition by holiday, marketing_region ORDER BY purchase_date asc ROWS BETWEEN 7 Preceding AND Current row) AS gc_receipts_7day,
+SUM(receipts)
+  OVER ( partition by holiday, marketing_region ORDER BY purchase_date asc ROWS BETWEEN 7 Preceding AND Current row) AS receipts_7day,
+avg( safe_divide(gc_receipts,receipts ) )
+  OVER ( partition by holiday, marketing_region ORDER BY purchase_date asc ROWS BETWEEN 60 Preceding AND Current row) AS gc_receipts_share_60day,
+avg( safe_divide(gc_receipts,receipts ) )
+  OVER ( partition by holiday, marketing_region ORDER BY purchase_date asc ROWS BETWEEN 7 Preceding AND Current row) AS gc_receipts_share_7day,
+from daily d)
+select *
+from daily_history
+where holiday is not null
+order by marketing_region, purchase_date asc;
+
+*/
